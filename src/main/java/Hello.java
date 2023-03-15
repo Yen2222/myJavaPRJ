5fb6611efcb50f4d17c1f3b3118b6559bf7986b6
data22 <lyebh1234@gmail.com>
2023-03-15 09:28:49 +0900

    initial

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[]args){
+        System.out.println("Hello!! My First Java Coding");
+        System.out.println("Hi, My name is YeEun!!");
+        System.out.println("Nice to meet you");
+        System.out.println("ya!!!!");
+    }
+}
---
+++ src/main/java/Star.java
@@ -0,0 +1,27 @@
+public class Star {
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        for (int i = 0; i < a*2; i++) {
+            if ( i < a) {
+                for (int j = 0; j < i; j++) {
+                    System.out.printf("*");
+                }
+
+            }
+
+            else {
+
+                for (int j = a; j > i-a; j--) {
+                    System.out.printf("*");
+                }
+
+
+            }
+            System.out.printf("\n");
+
+        }
+
+    }
+}
